04723a3196215cbc9fc4376b2637fce66f8d7708
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-06 16:13:28 +0600

    data models added

--- src/main.go
+++ src/main.go
@@ -1,5 +1,17 @@
 package main
 
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
 func main() {
 
 }
